c53cd7aeb3fe51e2af45176f8dab66835a45dc4a
Spimy <officialspyme@gmail.com>
2020-11-17 23:45:03 +0400

    Add search component

---
+++ src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    class="search"
+    type="text"
+    placeholder="Enter Anime Title..."
+    aria-label="Enter Anime Title..."
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Search"
+});
+</script>
+
+<style lang="scss" scoped>
+.search {
+  font-size: 1em;
+  font-family: "Berlin Sans FB Regular", sans-serif;
+  background-color: #3a3255;
+  border: none;
+  padding: 1rem 1.5rem;
+  color: #ffffff;
+}
+.search::placeholder {
+  color: rgba(255, 255, 255, 0.31);
+}
+</style>

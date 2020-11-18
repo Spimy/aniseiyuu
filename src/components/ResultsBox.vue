024c1c2c30a91a58a5843beefbd3fbf1cb94a498
Spimy <officialspyme@gmail.com>
2020-11-18 23:28:44 +0400

    Add results box component

---
+++ src/components/ResultsBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="results-box">
+    <div v-for="result in results" :key="result" class="result">
+      {{ result }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ResultsBox",
+  props: {
+    results: Array
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.results-box {
+  background-color: #292734;
+  width: 100%;
+  max-height: 10em;
+  overflow-y: auto;
+
+  .result {
+    cursor: pointer;
+    padding: 0.5em 1em;
+    width: 100%;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.05);
+    }
+  }
+}
+</style>

9c9230db4ee00e7ddf2640793e9cfcf62c59974b
Spimy <officialspyme@gmail.com>
2020-11-18 23:31:29 +0400

    Make search functional with use of results box

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,31 +1,76 @@
 <template>
-  <input
-    class="search"
-    type="text"
-    placeholder="Enter Anime Title..."
-    aria-label="Enter Anime Title..."
-  />
+  <div class="search">
+    <input
+      type="text"
+      placeholder="Enter Anime Title..."
+      aria-label="Enter Anime Title..."
+      v-model="query"
+      @keyup="search"
+      @blur="show = false"
+      @focus="show = true"
+    />
+    <transition name="fade">
+      <ResultsBox v-if="results.length > 0 && show" :results="results" />
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import ResultsBox from "@/components/ResultsBox.vue";
 
 export default defineComponent({
-  name: "Search"
+  name: "Search",
+  components: {
+    ResultsBox
+  },
+  setup() {
+    let timeoutRef: null | number = null;
+    const query = ref("");
+    const results = ref([] as Array<number>); // This is temporary for testing
+    const show = ref(false);
+
+    const search = () => {
+      if (timeoutRef !== null) {
+        clearTimeout(timeoutRef);
+      }
+
+      timeoutRef = setTimeout(() => {
+        if (query.value === "") {
+          results.value = [];
+          return;
+        }
+        if (results.value.length === 0) return results.value.push(0);
+        return results.value.push(results.value[results.value.length - 1] + 1);
+      }, 400);
+    };
+
+    return { query, results, show, search };
+  }
 });
 </script>
 
 <style lang="scss" scoped>
 .search {
-  font-size: 1em;
-  font-family: "Berlin Sans FB Regular", sans-serif;
-  background-color: var(--faded-purple);
-  border: none;
-  padding: 1rem 1.5rem;
-  color: #ffffff;
+  position: relative;
 
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.31);
+  input {
+    font-size: 1em;
+    font-family: "Berlin Sans FB Regular", sans-serif;
+    background-color: var(--faded-purple);
+    border: none;
+    padding: 1rem 1.5rem;
+    color: #ffffff;
+    width: 100%;
+
+    &::placeholder {
+      color: rgba(255, 255, 255, 0.31);
+    }
+  }
+
+  .results-box {
+    position: absolute;
+    z-index: 1;
   }
 }
 </style>

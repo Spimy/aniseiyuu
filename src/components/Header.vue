bd240ac9a5a446761d72254ee57229ea4f6c6a24
Spimy <officialspyme@gmail.com>
2020-11-18 20:25:53 +0400

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>AniSeiyuu</h1>
+    <Nav />
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Nav from "@/components/Nav.vue";
+
+export default defineComponent({
+  name: "header",
+  components: { Nav }
+});
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>

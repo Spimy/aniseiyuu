c92f470955b68468f432bf4f6f702f96ad17994d
Spimy <officialspyme@gmail.com>
2020-11-18 18:59:13 +0400

    Convert single to double quotes and add semicolon

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowsAltH } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowsAltH } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-
-library.add(faArrowsAltH)
+library.add(faArrowsAltH);
 
 createApp(App)
   .use(router)

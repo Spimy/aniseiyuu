b516bd04e9b3211f6642218e4809b80dbc0611ab
Spimy <officialspyme@gmail.com>
2020-11-21 22:21:18 +0400

    Display result fetched in anime box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,22 @@
 <template>
   <main>
     <div class="search-container">
-      <Search />
+      <Search @select-anime="setAnime($event, 0)" />
       <button><CompareArrow /><br />Seiyuu</button>
-      <Search />
+      <Search @select-anime="setAnime($event, 1)" />
     </div>
     <div class="result-container">
-      <AnimeBox :anime="anime" />
-      <AnimeBox :anime="anime" />
+      <AnimeBox v-for="anime in animeList" :key="anime" :anime="anime" />
     </div>
   </main>
-  <!-- <Modal /> -->
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, ref } from "vue";
 import Search from "@/components/Search.vue";
 import AnimeBox from "@/components/AnimeBox.vue";
 import CompareArrow from "@/assets/icons/CompareArrow.vue";
+import { IAnime } from "@/libs/interfaces/Anime";
 
 export default defineComponent({
   name: "Home",
@@ -27,24 +26,11 @@ export default defineComponent({
     AnimeBox
   },
   setup() {
-    const anime = reactive({
-      id: 9253,
-      title: {
-        romaji: "Steins;Gate",
-        english: "Steins;Gate",
-        native: "シュタインズ・ゲート"
-      },
-      description:
-        'Self-proclaimed mad scientist Okabe Rintarou lives in a small room in Akihabara, where he invents "future gadgets" with fellow lab members Shiina Mayuri, his air-headed childhood friend, and Hashida Itaru, an otaku hacker. The three pass the time by tinkering with their latest creation, a "Phone Microwave" that can be controlled through text messages. \n<br><br>\nThe lab members soon face a string of mysterious incidents that lead to a game-changing discovery: the Phone Microwave can send emails to the past and thus alter history. Adapted from the critically acclaimed visual novel by 5pb. and Nitroplus, Steins;Gate takes Okabe to the depths of scientific theory and human despair as he faces the dire consequences of changing the past.\n<br><br>\n(Summary by Edo)',
-      coverImage: {
-        extraLarge:
-          "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx9253-7pdcVzQSkKxT.jpg"
-      },
-      bannerImage:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/banner/n9253-JIhmKgBKsWUN.jpg",
-      siteUrl: "https://anilist.co/anime/9253"
-    });
-    return { anime };
+    const animeList = ref([] as Array<IAnime>);
+    const setAnime = (anime: IAnime, index: 0 | 1) => {
+      animeList.value[index] = anime;
+    };
+    return { animeList, setAnime };
   }
 });
 </script>

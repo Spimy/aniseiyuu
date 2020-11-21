e8764bd471ae6fec48280ad619ab975bc4cdc539
Spimy <officialspyme@gmail.com>
2020-11-21 21:17:57 +0400

    Use anime box component with test data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,46 @@
       <button><CompareArrow /><br />Seiyuu</button>
       <Search />
     </div>
+    <div class="result-container">
+      <AnimeBox :anime="anime" />
+      <AnimeBox :anime="anime" />
+    </div>
   </main>
+  <!-- <Modal /> -->
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 import Search from "@/components/Search.vue";
+import AnimeBox from "@/components/AnimeBox.vue";
 import CompareArrow from "@/assets/icons/CompareArrow.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     Search,
-    CompareArrow
+    CompareArrow,
+    AnimeBox
+  },
+  setup() {
+    const anime = reactive({
+      id: 9253,
+      title: {
+        romaji: "Steins;Gate",
+        english: "Steins;Gate",
+        native: "シュタインズ・ゲート"
+      },
+      description:
+        'Self-proclaimed mad scientist Okabe Rintarou lives in a small room in Akihabara, where he invents "future gadgets" with fellow lab members Shiina Mayuri, his air-headed childhood friend, and Hashida Itaru, an otaku hacker. The three pass the time by tinkering with their latest creation, a "Phone Microwave" that can be controlled through text messages. \n<br><br>\nThe lab members soon face a string of mysterious incidents that lead to a game-changing discovery: the Phone Microwave can send emails to the past and thus alter history. Adapted from the critically acclaimed visual novel by 5pb. and Nitroplus, Steins;Gate takes Okabe to the depths of scientific theory and human despair as he faces the dire consequences of changing the past.\n<br><br>\n(Summary by Edo)',
+      coverImage: {
+        extraLarge:
+          "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx9253-7pdcVzQSkKxT.jpg"
+      },
+      bannerImage:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/banner/n9253-JIhmKgBKsWUN.jpg",
+      siteUrl: "https://anilist.co/anime/9253"
+    });
+    return { anime };
   }
 });
 </script>
@@ -54,6 +81,16 @@ export default defineComponent({
     button {
       width: 100%;
     }
+  }
+}
+
+.result-container {
+  margin: 3em 0;
+  display: flex;
+  gap: 1em;
+
+  @media (max-width: 960px) {
+    flex-direction: column;
   }
 }
 </style>

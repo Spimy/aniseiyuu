53644d0a50c42ff64e3cd15dae4ae45d1a3d15ea
Spimy <officialspyme@gmail.com>
2020-11-29 17:42:45 +0400

    Add contact vue

---
+++ src/views/Contact.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>👨‍💻 Maintainer</h2>
+    <p>If you have:</p>
+    <ul>
+      <li>A question about something</li>
+      <li>Found a bug somewhere</li>
+      <li>Something else on your mind</li>
+    </ul>
+    <p>
+      You can email us at one of the addresses down here:
+      <a href="mailto:williamlaw.3001@gmail.com">williamlaw.3001@gmail.com</a>
+    </p>
+  </div>
+  <div>
+    <h2>⌨️ repository</h2>
+    <p>
+      We are open source btw! You can visit our repo on
+      <a href="https://github.com/Spimy/aniseiyuu" target="_blank">GitHub</a> if
+      you want to look around or even contribute 😃.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  max-width: 50ch;
+  margin: 2em auto;
+
+  ul {
+    list-style-type: square;
+  }
+
+  p {
+    margin: 1em 0;
+    line-height: 1.5em;
+  }
+}
+</style>

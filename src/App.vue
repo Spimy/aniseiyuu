fa2342373d15f720bddfcb27ab7deda8a6ebead0
Spimy <officialspyme@gmail.com>
2020-11-29 16:28:50 +0400

    Use footer component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <Header />
   <router-view />
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
 export default defineComponent({
-  components: { Header }
+  components: { Header, Footer }
 });
 </script>
 

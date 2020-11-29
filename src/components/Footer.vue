96cfcede67a004592665965b904480d63d5c30dd
Spimy <officialspyme@gmail.com>
2020-11-29 16:28:41 +0400

    Add sticky footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    Copyright © 2020 - AniSeiyuu by Spimy
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Footer"
+});
+</script>
+
+<style lang="scss" scoped>
+footer {
+  text-align: center;
+  padding: 0.5em;
+  position: fixed;
+  bottom: 0;
+  left: 7em;
+  right: 7em;
+  background-color: var(--faded-purple);
+
+  @media (max-width: 680px) {
+    left: 2em;
+    right: 2em;
+  }
+}
+</style>
